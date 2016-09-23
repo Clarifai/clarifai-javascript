99c17dbabf9b952356ef2bddb4f5de2e03c99677
Jade Misenas <jade@clarifai.com>
2016-09-23 12:37:02 -0400

    Add public model ids

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ let App = require('./src/App');
 module.exports = global.Clarifai = {
   App,
   GENERAL_MODEL: "aaa03c23b3724a16a56b629203edc62c",
-  EMBEDDING_MODEL: "bbb5f41425b8468d9b7a554ff10f8581",
+  FOOD_MODEL: "bd367be194cf45149e75f01d59f77ba7",
+  TRAVEL_MODEL: "eee28c313d69466f836ab83287a54ed9",
+  NSFW_MODEL: "e9576d86d2004ed1a38ba0cf39ecb4b1",
+  WEDDINGS_MODEL: "c386b7a870114f4a87477c0824499348",
   COLOR_MODEL: "eeed0b6733a644cea07cf4c60f87ebb7",
-  FACE_DETECTION_MODEL: "fffeadbae88d471996fe31add17cfc1c",
   CLUSTER_MODEL: "cccbe437d6e54e2bb911c6aa292fb072",
   BLUR: "ddd9d34872ab32be9f0e3b2b98a87be2"
 };
