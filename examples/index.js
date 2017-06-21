0a95a529a5aab32a8397501e40a6c7afd29a8fdc
Jimmy Hickey <jimmy@clarifai.com>
2017-06-21 16:48:45 -0400

    Add more feedback tests and some test cleanup

--- examples/index.js
+++ examples/index.js
@@ -12,11 +12,13 @@ function log(d) {
   }
 }
 
-// clarifai.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/3o6gb3kkXfLvdKEZs4.gif', {video: true})
-//   .then(log)
-//   .catch(log);
+// Prediction on general model
+clarifai.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/3o6gb3kkXfLvdKEZs4.gif', {video: true})
+  .then(log)
+  .catch(log);
 
 
+// Provide feedback
 clarifai.models.feedback(Clarifai.GENERAL_MODEL, 'https://s3.amazonaws.com/samples.clarifai.com/metro-north.jpg', {
   id: 'xyz',
   data: {
@@ -26,32 +28,10 @@ clarifai.models.feedback(Clarifai.GENERAL_MODEL, 'https://s3.amazonaws.com/sampl
     ]
   },
   info: {
-    'end_user_id': 'xyz123',
-    'session_id': 'tdrfsf',
-    'event_type': 'annotation',
-    'output_id': '{{output_id}}'
+    'endUserId': '{end_user_id}',
+    'sessionId': '{{session_id}}',
+    'outputId': '{{output_id}}'
   }
 })
   .then(log)
   .catch(log);
-
-const a = {
-  'input': {
-    'id': 'xyz',
-    'data': {
-      'image': {
-        'url': ''
-      },
-      'concepts': [
-        {'id': 'mattid2', 'value': true},
-        {'id': 'lambo', 'value': false}
-      ]
-    },
-    'feedback_info': {
-      'end_user_id': 'xyz123',
-      'session_id': '',
-      'event_type': 'annotation',
-      'output_id': '{{output_id}}'
-    }
-  }
-};
