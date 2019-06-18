e16a0c42979a07221db994b7ec43e1bb0d1e8f9e
Rok Povsic <rok.povsic@gmail.com>
2019-06-18 10:10:48 +0200

    Remove 'export' usage

--- tests/assets/test-data.js
+++ tests/assets/test-data.js
@@ -1,6 +1,6 @@
-export const TINY_IMAGE_BASE64 = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z/C/HgAGgwJ/lK3Q6wAAAABJRU5ErkJggg==';
+const TINY_IMAGE_BASE64 = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z/C/HgAGgwJ/lK3Q6wAAAABJRU5ErkJggg==';
 
-export const sampleImages = [
+const sampleImages = [
   'https://s3.amazonaws.com/samples.clarifai.com/metro-north.jpg',
   'https://s3.amazonaws.com/samples.clarifai.com/wedding.jpg',
   'https://s3.amazonaws.com/samples.clarifai.com/cookies.jpeg',
@@ -12,7 +12,9 @@ export const sampleImages = [
   'https://s3.amazonaws.com/samples.clarifai.com/black-car.jpg'
 ];
 
-export const sampleVideos = [
+const sampleVideos = [
   'https://samples.clarifai.com/3o6gb3kkXfLvdKEZs4.gif',
   'https://samples.clarifai.com/beer.mp4'
 ];
+
+module.exports = {sampleImages, sampleVideos, TINY_IMAGE_BASE64};
--- tests/unit/helpers.js
+++ tests/unit/helpers.js
@@ -1,2 +1,4 @@
-export const BASE_URL = 'https://api.clarifai.com';
-export const SAMPLE_API_KEY = 'some-sample-API-key';
+const BASE_URL = 'https://api.clarifai.com';
+const SAMPLE_API_KEY = 'some-sample-API-key';
+
+module.exports = {BASE_URL, SAMPLE_API_KEY};
