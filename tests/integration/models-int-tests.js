7860dc92a6c2e8148a93777921df83495eb51d39
Tianyao Luo <luke@clarifai.com>
2020-03-09 21:32:45 -0400

    check if eval status is in(21300, 21301, 21303)

--- tests/integration/models-int-tests.js
+++ tests/integration/models-int-tests.js
@@ -137,7 +137,7 @@ describe('Integration Tests - Models', () => {
         expect(modelVersion).toBeDefined();
         expect(modelVersion.metrics).toBeDefined();
         expect(modelVersion.metrics.status).toBeDefined();
-        expect(modelVersion.metrics.status.code).toBe(21303);
+        expect([21300, 21301, 21303]).toContain(modelVersion.metrics.status.code);
         done();
       })
       .catch(errorHandler.bind(done));
